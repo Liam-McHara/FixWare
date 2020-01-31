a041ca127080c83352dea4dd4be7b515792292ff
Liam_McHara <guillemlr@pm.me>
2020-01-31 21:49:24 +0100

    Menú basico con iniciador minijuegos random

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int vidas;
+    public int victorias;
+    // minijuegos_jugados
+    // dificultad / tiempo_limite
+    //public GameScene[] minijuegos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Click()
+    {
+
+    }
+
+
+}
---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public string[] minijuegos;
+
+    public void EnterGameplay()
+    {
+        int random = Random.Range(0, minijuegos.Length);
+        SceneManager.LoadScene(minijuegos[random]);
+    }
+}
