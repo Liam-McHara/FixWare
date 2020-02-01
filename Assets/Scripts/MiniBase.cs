6f85529db3714c0d4964776f5367dbb0c3943100
Liam_McHara <guillemlr@pm.me>
2020-02-01 16:29:10 +0100

    Añadidos tiempos espera

--- Assets/Scripts/MiniBase.cs
+++ Assets/Scripts/MiniBase.cs
@@ -10,10 +10,16 @@ public class MiniBase : MonoBehaviour
     public GameController gameController;
     public float preTitleTime = 1.0f;
     public float preGameTime = 1.0f;
-    float tiempo = 5;        // Tiempo (segundos) para resolver el minijuego (se va reduciendo)
+    public SpriteRenderer check;
+    public SpriteRenderer cross;
+    float tiempo;                   // Tiempo (segundos) para resolver el minijuego (se va reduciendo)
     float tiempoInicial;            // Tiempo (segundos) para resolver el minijuego (guarda el valor inicial)
     public bool win = false;        // Indica si se ha resuelto el minijuego
     public bool fail = false;       // Indica si se ha fallado el minijuego
+
+    bool titleShown = false;
+    bool gameStarted = false;
+    float interTimer = 0.0f;        
 
 
     // UI elements (timer, win sprite, fail sprite...)
@@ -21,6 +27,8 @@ public class MiniBase : MonoBehaviour
 
     void Start()
     {
+        check.enabled = false;
+        cross.enabled = false;
         gameController = GameObject.Find("GameController").GetComponent<GameController>();
         tiempo = gameController.tiempo;
         tiempoInicial = tiempo; 
@@ -34,17 +42,52 @@ public class MiniBase : MonoBehaviour
 
     void UpdateTimer()  
     {
-        if (!win && !fail) tiempo -= Time.deltaTime;    // El tiempo corre mientras no se haya terminado el minijuego
-        if (tiempo < 0) Fail();                         // Time's up!
+        // PRE-GAME
+        if (!titleShown)
+        {
+            interTimer += Time.deltaTime;
+            if (interTimer >= preTitleTime)
+            {
+                ShowTitle();
+                interTimer = 0;
+            }
+        }
+        else if (!gameStarted)
+        {
+            interTimer += Time.deltaTime;
+            if (interTimer >= preGameTime)
+            {
+                StartGame();
+            }
+        }
 
-        // Actualizar UI
-        timerBar.fillAmount = tiempo / tiempoInicial;
+        // IN-GAME
+        else if (gameStarted)
+        {
+            if (!win && !fail) tiempo -= Time.deltaTime;    // El tiempo corre mientras no se haya terminado el minijuego
+            if (tiempo < 0) Fail();                         // Time's up!
+
+            // Actualizar UI
+            timerBar.fillAmount = tiempo / tiempoInicial;
+        }
     }
 
     public void SetTime(float t)
     {
         tiempo = t;
         tiempoInicial = tiempo;
+    }
+
+    void ShowTitle()
+    {
+        Debug.Log("ShowTitle :D");
+        titleShown = true;
+    }
+
+    void StartGame()
+    {
+        Debug.Log("Start Game :D");
+        gameStarted = true;
     }
 
     // para provocar eventos de victoria o derrota desde otros scripts
