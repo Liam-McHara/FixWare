d0d3e0c489ee8edd639558088d0d000be93b55a3
Liam_McHara <guillemlr@pm.me>
2020-02-01 14:08:56 +0100

    Puzzle 1.0

--- Assets/Scripts/#Base para Minijuegos.cs
+++ Assets/Scripts/#Base para Minijuegos.cs
@@ -4,14 +4,11 @@ using UnityEngine;
 
 public class SCRIPT_NAME_HERE : MonoBehaviour
 {
-
     MiniBase miniBase;
 
-    // Start is called before the first frame update
     void Start()
     {
         miniBase = gameObject.GetComponentInParent<MiniBase>();
-
     }
 
     void Update()
---
+++ Assets/Scripts/Puzzle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle : MonoBehaviour
+{
+    public Draggable[] piezas;  // Piezas del puzzle
+    MiniBase miniBase;
+
+    void Start()
+    {
+        miniBase = gameObject.GetComponentInParent<MiniBase>();
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < piezas.Length; ++i)
+        {
+            if (!piezas[i].guachi) break;
+            miniBase.Win();
+            Debug.Log("COMPLETADO!");
+        }
+    }
+}
