bd04ae16d9b4fedc97b6acc6ab2d77940dbad1cd
Liam_McHara <guillemlr@pm.me>
2020-02-01 21:06:15 +0100

    begin changetitle

---
+++ Assets/Scripts/ChangeTitle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeTitle : MonoBehaviour
+{
+    public Text titulo;
+    public MiniBase miniBase;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
